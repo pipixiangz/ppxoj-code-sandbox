8111ab312b235f7804764b647535ab2ef02ada13
pipixiangz <763038134@qq.com>
2024-08-07 14:44:00 +0800

    bug fixed

--- src/main/java/com/ppx/ppxojcodesandbox/CodeSandboxTemplate.java
+++ src/main/java/com/ppx/ppxojcodesandbox/CodeSandboxTemplate.java
@@ -3,6 +3,7 @@ package com.ppx.ppxojcodesandbox;
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.ObjectUtil;
 import com.ppx.ppxojcodesandbox.model.*;
+import com.ppx.ppxojcodesandbox.model.enums.JudgeInfoMessageEnum;
 import com.ppx.ppxojcodesandbox.utils.ProcessUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.StopWatch;
@@ -18,64 +19,46 @@ import java.util.UUID;
 /**
  * 代码沙箱模板
  */
+import java.util.concurrent.TimeUnit;
+
 @Slf4j
 public abstract class CodeSandboxTemplate implements CodeSandbox {
 
-    String prefix;
-    String globalCodeDirPath;
-    String globalCodeFileName;
+    protected String prefix;
+    protected String globalCodeDirPath;
+    protected String globalCodeFileName;
+
+    // 使用常量替代魔法数字
+    private static final long DEFAULT_TIMEOUT_MS = 10000L;
+    private static final int TIMEOUT_EXIT_CODE = -10001;
 
     /**
-     * 超时时间，超过10秒则结束
-     */
-    public static final Long DEFAULT_TIME_OUT = 10000L;
-
-    /**
-     * 每个实现类必须实现编译以及运行的cmd
-     *
+     * 获取编译和运行的命令
      * @param userCodeParentPath 代码所在的父目录
      * @param userCodePath 代码所在目录
-     * @return
+     * @return 编译和运行的命令
      */
-    abstract CodeSandboxCmd getCmd(String userCodeParentPath, String userCodePath);
+    protected abstract CodeSandboxCmd getCmd(String userCodeParentPath, String userCodePath);
 
     /**
-     * 每个实现类可以选择性实现运行沙箱命令的方法
-     *
-     * @param cmd 编译和运行命令
-     * @throws IOException
-     * @throws InterruptedException
-     */
-    public void runSandboxCmd(CodeSandboxCmd cmd) throws IOException, InterruptedException {
-        // 默认实现为空，子类可以选择性地覆盖这个方法
-    }
-
-    /**
-     * 保存代码到文件中，不同编程语言要放到不同文件夹中
-     * 保存到文件中的格式为: UUID/代码文件
-     *
-     * @param code 代码
-     * @return
+     * 保存代码到文件
+     * @param code 代码内容
+     * @return 保存代码的文件
      */
     private File saveCodeToFile(String code) {
         String globalCodePath = System.getProperty("user.dir") + globalCodeDirPath;
-        if (!FileUtil.exist(globalCodePath)) {
-            FileUtil.mkdir(globalCodePath);
-        }
+        FileUtil.mkdir(globalCodePath);
 
-        // 存放用户代码
         String userCodeParentPath = globalCodePath + prefix + File.separator + UUID.randomUUID();
         String userCodePath = userCodeParentPath + globalCodeFileName;
-
         return FileUtil.writeString(code, userCodePath, StandardCharsets.UTF_8);
     }
 
     /**
-     * 编译代码，返回编译的信息
-     *
+     * 编译代码
      * @param compileCmd 编译命令
-     * @return
-     * @throws IOException IOException
+     * @return 编译结果
+     * @throws IOException 编译过程中的IO异常
      */
     private ExecuteMessage compileCode(String compileCmd) throws IOException {
         Process compileProcess = Runtime.getRuntime().exec(compileCmd);
@@ -84,110 +67,139 @@ public abstract class CodeSandboxTemplate implements CodeSandbox {
 
     /**
      * 运行代码
-     *
-     * @param inputList 输入用例
-     * @param runCmd 运行的cmd
-     * @return
-     * @throws RuntimeException RuntimeException
+     * @param inputList 输入用例列表
+     * @param runCmd 运行命令
+     * @return 运行结果列表
      */
-    private List<ExecuteMessage> runCode(List<String> inputList, String runCmd) throws RuntimeException {
-        List<ExecuteMessage> executeMessageList = new LinkedList<>();
+    private List<ExecuteMessage> runCode(List<String> inputList, String runCmd) {
+        List<ExecuteMessage> executeMessageList = new ArrayList<>();
         for (String input : inputList) {
-            Process runProcess;
             try {
-                runProcess = Runtime.getRuntime().exec(runCmd);
-                new Thread(() -> {
-                    try {
-                        Thread.sleep(DEFAULT_TIME_OUT);
-                        log.info("超时了，中断");
-                        runProcess.destroy();
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }).start();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+                Process runProcess = Runtime.getRuntime().exec(runCmd);
+                ExecuteMessage executeMessage = runSingleCase(runProcess, input);
+                executeMessageList.add(executeMessage);
 
-            StopWatch stopWatch = new StopWatch();
-            stopWatch.start();
-            ExecuteMessage executeMessage = ProcessUtil.handleProcessInteraction(runProcess, input, "运行");
-            stopWatch.stop();
-            executeMessage.setTime(stopWatch.getLastTaskTimeMillis());
-            executeMessageList.add(executeMessage);
+                if (executeMessage.getExitValue() != 0) {
+                    break;
+                }
+            } catch (IOException | InterruptedException e) {
+                log.error("运行代码出错", e);
+                executeMessageList.add(createErrorMessage(e));
+                break;
+            }
         }
         return executeMessageList;
     }
 
     /**
-     * 执行代码
-     * @param executeCodeRequest
-     * @return
+     * 运行单个测试用例
+     * @param runProcess 运行进程
+     * @param input 输入
+     * @return 运行结果
      */
+    private ExecuteMessage runSingleCase(Process runProcess, String input) throws InterruptedException {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        ExecuteMessage executeMessage = ProcessUtil.handleProcessInteraction(runProcess, input, "运行");
+
+        boolean finished = runProcess.waitFor(DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS);
+        stopWatch.stop();
+
+        if (!finished) {
+            runProcess.destroy();
+            return ExecuteMessage.builder()
+                    .exitValue(TIMEOUT_EXIT_CODE)
+                    .errorMessage("超时")
+                    .time(DEFAULT_TIMEOUT_MS)
+                    .build();
+        }
+
+        executeMessage.setTime(stopWatch.getLastTaskTimeMillis());
+        return executeMessage;
+    }
+
     @Override
     public final ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
-        List<String> inputList = executeCodeRequest.getInputList();
-        String code = executeCodeRequest.getCode();
-
-        // 保存代码
-        File userCodeFile = saveCodeToFile(code);
+        File userCodeFile = saveCodeToFile(executeCodeRequest.getCode());
         String userCodePath = userCodeFile.getAbsolutePath();
         String userCodeParentPath = userCodeFile.getParentFile().getAbsolutePath();
 
-        CodeSandboxCmd cmd = getCmd(userCodeParentPath, userCodePath);
-        String compileCmd = cmd.getCompileCmd();
-        String runCmd = cmd.getRunCmd();
-
-        // 编译代码
         try {
-            ExecuteMessage executeMessage = compileCode(compileCmd);
-            if (executeMessage.getExitValue() != 0) {
-                FileUtil.del(userCodeParentPath);
-                return ExecuteCodeResponse.builder().status(2).message("编译错误").build();
-            }
-        } catch (IOException e) {
-            FileUtil.del(userCodeParentPath);
-            return errorResponse(e);
-        }
+            CodeSandboxCmd cmd = getCmd(userCodeParentPath, userCodePath);
+            ExecuteMessage compileMessage = compileCode(cmd.getCompileCmd());
 
-        // 执行代码
-        try {
-            runSandboxCmd(cmd);
-            List<ExecuteMessage> executeMessageList = runCode(inputList, runCmd);
-
-            // 返回处理结果
-            ExecuteCodeResponse executeCodeResponse = new ExecuteCodeResponse();
-            executeCodeResponse.setStatus(1);
-            JudgeInfo judgeInfo = new JudgeInfo();
-            executeCodeResponse.setJudgeInfo(judgeInfo);
-            List<String> outputList = new LinkedList<>();
-            long maxTime = 0;
-            for (ExecuteMessage executeMessage : executeMessageList) {
-                if (ObjectUtil.equal(0, executeMessage.getExitValue())) {
-                    outputList.add(executeMessage.getMessage());
-                } else {
-                    executeCodeResponse.setMessage(executeMessage.getErrorMessage());
-                    executeCodeResponse.setStatus(3);
-                    break;
-                }
-                maxTime = Math.max(maxTime, executeMessage.getTime());
+            if (compileMessage.getExitValue() != 0) {
+                return createErrorResponse(2, "编译错误", JudgeInfoMessageEnum.COMPILE_ERROR);
             }
-            judgeInfo.setTime(maxTime);
-            executeCodeResponse.setOutputList(outputList);
+
+            List<ExecuteMessage> runMessages = runCode(executeCodeRequest.getInputList(), cmd.getRunCmd());
+            return processRunResults(runMessages);
+        } catch (Exception e) {
+            log.error("执行代码出错", e);
+            return createErrorResponse(2, e.getMessage(), JudgeInfoMessageEnum.SYSTEM_ERROR);
+        } finally {
             FileUtil.del(userCodeParentPath);
-            return executeCodeResponse;
-        } catch (RuntimeException | IOException | InterruptedException e) {
-            FileUtil.del(userCodeParentPath);
-            return errorResponse(e);
         }
     }
 
     /**
-     * 错误返回
-     * @param e
-     * @return
+     * 处理运行结果
+     * @param executeMessageList 运行结果列表
+     * @return 执行代码的响应
      */
-    private ExecuteCodeResponse errorResponse(Throwable e) {
-        return ExecuteCodeResponse.builder().outputList(new ArrayList<>()).message(e.getMessage()).judgeInfo(new JudgeInfo()).status(2).build();
+    private ExecuteCodeResponse processRunResults(List<ExecuteMessage> executeMessageList) {
+        ExecuteCodeResponse response = new ExecuteCodeResponse();
+        response.setStatus(1);
+
+
+        JudgeInfo judgeInfo = new JudgeInfo();
+        List<String> outputList = new ArrayList<>();
+        long maxTime = 0;
+
+        for (ExecuteMessage executeMessage : executeMessageList) {
+            if (executeMessage.getExitValue() == 0) {
+                outputList.add(executeMessage.getMessage());
+                maxTime = Math.max(maxTime, executeMessage.getTime());
+            } else {
+                return createErrorResponse(3, executeMessage.getErrorMessage(),
+                        executeMessage.getExitValue() == TIMEOUT_EXIT_CODE ?
+                                JudgeInfoMessageEnum.TIME_LIMIT_EXCEEDED :
+                                JudgeInfoMessageEnum.RUNTIME_ERROR);
+            }
+        }
+
+        judgeInfo.setTime(maxTime);
+        response.setJudgeInfo(judgeInfo);
+        response.setOutputList(outputList);
+        return response;
+    }
+
+    /**
+     * 创建错误响应
+     * @param status 状态码
+     * @param message 错误消息
+     * @param resultType 结果类型
+     * @return 错误响应
+     */
+    private ExecuteCodeResponse createErrorResponse(int status, String message, JudgeInfoMessageEnum resultType) {
+        return ExecuteCodeResponse.builder()
+                .status(status)
+                .message(message)
+                .outputList(new ArrayList<>())
+                .judgeInfo(new JudgeInfo())
+                .build();
+    }
+
+    /**
+     * 创建错误消息
+     * @param e 异常
+     * @return 错误消息
+     */
+    private ExecuteMessage createErrorMessage(Exception e) {
+        return ExecuteMessage.builder()
+                .exitValue(-1)
+                .errorMessage(e.getMessage())
+                .build();
     }
 }
